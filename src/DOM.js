1cd23cd0429b57231c3ec54d27f280bec6085c0c
bickyy <vicky.zhou21@sitechhs.com>
2020-11-10 16:02:34 -0500

    nksjdb

---
+++ src/DOM.js
@@ -0,0 +1,13 @@
+const DOMSelectors = {
+  breed: document.querySelector(".breed"),
+};
+
+// const DOMSelectors = {
+//     grid: document.querySelector(".movie-grid"),
+//     searchForm: document.getElementById("search-form"),
+//     searchArea: document.getElementById("search-area"),
+//     btnPrev: document.querySelector(".btn-prev"),
+//     btnNext: document.querySelector(".btn-next"),
+//   };
+
+export { DOMSelectors };
--- src/index.js
+++ src/index.js
@@ -3,3 +3,29 @@
 //pagination
 //display results
 //init
+
+import { DOMSelectors } from "./DOM";
+
+let regeneratorRuntime = require("regenerator-runtime");
+
+const key = `8e70c3ac-4bf9-4f4c-91b1-da53ffd062eb`;
+const query = `https://api.TheDogAPI.com/v1/breeds?api_key=${key}`;
+
+const init = async function () {
+  try {
+    const response = await fetch(query);
+    const data = await response.json();
+    console.log(data);
+    data.results.forEach((breeds) => {
+      DOMSelectors.breed.insertAdjacentHTML(
+        "beforeend",
+
+        `<p class="breed-list">${breeds.name}</p>`
+      );
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+init();
