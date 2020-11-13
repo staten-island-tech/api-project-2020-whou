918cffa7ca169a52fd84f5366303a7f8a58e8182
colleenww <colleenwhwang@gmail.com>
2020-11-12 22:45:06 -0500

    trying old api

--- src/index.js
+++ src/index.js
@@ -1,31 +1,44 @@
-//import stuff (DOM)
-//const API key
-//pagination
-//display results
-//init
+// //import stuff (DOM)
+// //const API key
+// //pagination
+// //display results
+// //init
 
-import { DOMSelectors } from "./DOM";
+// import { DOMSelectors } from "./DOM";
 
-let regeneratorRuntime = require("regenerator-runtime");
+// let regeneratorRuntime = require("regenerator-runtime");
 
-const key = `8e70c3ac-4bf9-4f4c-91b1-da53ffd062eb`;
-const query = `https://api.TheDogAPI.com/v1/breeds?api_key=${key}`;
+// const key = `8e70c3ac-4bf9-4f4c-91b1-da53ffd062eb`;
+// const query = `https://api.TheDogAPI.com/v1/breeds?api_key=${key}`;
 
-const init = async function () {
-  try {
-    const response = await fetch(query);
-    const data = await response.json();
-    console.log(data);
-    data.results.forEach((breeds) => {
-      DOMSelectors.breed.insertAdjacentHTML(
-        "beforeend",
+// const init = async function () {
+//   try {
+//     const response = await fetch(query);
+//     const data = await response.json();
+//     console.log(data);
+//     data.results.forEach((breeds) => {
+//       DOMSelectors.breed.insertAdjacentHTML(
+//         "beforeend",
 
-        `<p class="breed-list">${breeds.name}</p>`
-      );
-    });
-  } catch (error) {
-    console.log(error);
-  }
-};
+//         `<p class="breed-list">${breeds.name}</p>`
+//       );
+//     });
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
 
-init();
+// init();
+
+const breedsURL = "https://dog.ceo/api/breeds/list/all";
+
+fetch(breedsURL)
+  .then((respond) => {
+    return respond.json();
+  })
+  .then((data) => {
+    const breedsObject = data.message;
+    const breedsArray = Object.keys(breedsObject);
+    for (let i = 0; i < breedsArray.length; i++) {}
+    console.log(breedsArray);
+  });
